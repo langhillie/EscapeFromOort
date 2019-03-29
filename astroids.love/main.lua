d53e4d7d0b45ff35a08a8302357116ab5e925fc3
Justin Langille <Justin.Langille@dal.ca>
2019-03-29 15:26:05 -0300

    reworked ship, added looping, and more

--- astroids.love/main.lua
+++ astroids.love/main.lua
@@ -1,6 +1,53 @@
 function debug()
 	love.graphics.print("Weapon Cooldown: " .. player.weapon.cooldown, 5, 5)
 
+	-- reset ship
+	if love.keyboard.isDown("r") then
+		player.x = window.width / 2
+		player.y = window.height / 2
+		player.xVelocity = 0
+		player.yVelocity = 0
+		player.rot = 0
+	end
+	
+	--for i,v in pairs(player.bullets) do
+		-- print debug information
+	--end
+end
+
+-- TODO
+-- mid point subdivision
+function generateAsteroid(x, y, s)
+
+	love.graphics.line(x, y - 4, x - 4, y, x, y + 4, x + 4, y)
+
+--[[
+	roughness = 12
+	
+	points = {}
+	rand = math.rand()
+	pointx[0] = s + rand * roughness
+	for i=0, N do
+		for j=0, M do
+			points[2*j] = {}
+			points[2*j].x = 0
+			points[2*j].y = 0
+		end
+	
+	end
+
+	love.graphics.line()
+	]]--
+end
+
+
+function drawUI()
+
+end
+
+-- returns false if co-ordinates are out of bounds (and object should be deleted)
+function checkBounds(x, y)
+
 end
 
 function drawShip()
@@ -14,6 +61,13 @@ function drawShip()
 	love.graphics.translate(-player.x, -player.y)
 	-- Draws player
 	love.graphics.line(player.x - 5, player.y + 8, player.x, player.y - 8, player.x + 5, player.y + 8)
+	love.graphics.line(player.x - 2.5, player.y + 4, player.x + 2.5, player.y + 4)
+	if player.thrustEnabled == 1 then
+		rand = math.random()
+		if rand > 0.66 then
+			love.graphics.line(player.x - 2.5, player.y + 4, player.x, player.y + 15, player.x + 2.5, player.y + 4)
+		end
+	end
 	-- Restores the scenes matrix transformation
 	love.graphics.pop() 
 end
@@ -32,8 +86,14 @@ end
 -- Called on load
 function love.load()
 	-- variables
-	enableDebug = 1
-
+	settings = {}
+	settings.enableDebug = 1
+	game = {}
+	game.score = 0
+	
+	window = {}
+	window.width = love.graphics.getWidth()
+	window.height = love.graphics.getHeight()
 
 	-- player setup
 	player = {}
@@ -51,7 +111,7 @@ function love.load()
 	player.rot = 0 -- Initial rotation
 	
 	player.weapon = {}
-	player.weapon.velocity = 500
+	player.weapon.velocity = 600
 	player.weapon.firerate = 8 -- attacks per second
 	player.weapon.cooldown = 0
 	
@@ -62,19 +122,32 @@ function love.load()
 		bullet.x = player.x
 		bullet.y = player.y
 		bullet.rot = player.rot
-		bullet.xVelocity = (player.weapon.velocity + player.xVelocity) * math.sin(player.rot)
-		bullet.yVelocity = (player.weapon.velocity + player.yVelocity) * -math.cos(player.rot)
+		bullet.xVelocity = player.weapon.velocity * math.sin(player.rot) + player.xVelocity
+		bullet.yVelocity = player.weapon.velocity * -math.cos(player.rot) + player.yVelocity
 		table.insert(player.bullets, bullet)
 	end
 end
 
 -- Called before draw (once per frame)
 function love.update(dt)
-	if love.keyboard.isDown("o") then
-		enableDebug = 1 - enableDebug
+	-- looping ship at edges
+	if player.x > window.width then
+		player.x = 0
+	elseif player.x < 0 then
+		player.x = window.width
 	end
-	if enableDebug == 1 then
-		debug()
+	if player.y > window.height then
+		player.y = 0
+	elseif player.y < 0 then
+		player.y = window.height
+	end
+	
+	
+	if love.keyboard.isDown("o") then
+		settings.enableDebug = 1 - settings.enableDebug
+	end
+	if settings.enableDebug == 1 then
+		debug()	
 	end
 
 	-- Player Controls
@@ -106,9 +179,15 @@ function love.update(dt)
 
 	-- Update bullet locations
 	for i,v in pairs(player.bullets) do
+		-- delete bullet if it is out of bounds
+	
 		v.x = v.x + v.xVelocity * dt
 		v.y = v.y + v.yVelocity * dt
 	end
+	
+	
+	generateAsteroid(100, 100, 1)
+	
 end
 
 -- Draws the scene
